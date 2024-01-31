06a6eb72588acdad503c6c4d469c99b7892d2247
Freek Berson <fberson@ziggo.nl>
2024-01-31 16:34:58 +0100

    Update RAS_Azure_MP_Primary_CB.ps1

--- RAS_Azure_MP_Primary_CB.ps1
+++ RAS_Azure_MP_Primary_CB.ps1
@@ -136,12 +136,6 @@ WriteLog "Configuring Firewall Rules"
 New-NetFirewallRule -DisplayName "Parallels RAS Administration (TCP)" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 68, 80, 81, 1234, 135, 443, 445, 20001, 20002, 20003, 20009, 20020, 20030, 20443, 30004, 30006
 New-NetFirewallRule -DisplayName "Parallels RAS Administration (TCP)" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 80, 443, 20000, 20009, 30004, 30006
 
-#Disable UAC & Sharing Wizard to allow Remote Install of RAS Agent
-WriteLog "Disable UAC & Sharing Wizard"
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn -Name CheckedValue -Value 0
-
 #Download the latest RAS installer
 WriteLog "Dowloading most recent Parallels RAS Installer"
 $RASMedia = New-Object net.webclient
