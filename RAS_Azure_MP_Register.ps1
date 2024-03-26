b9770685b35514114f7ea27f3f078fab862c7a35
Freek Berson <fberson@ziggo.nl>
2024-03-26 15:38:23 +0100

    Update RAS_Azure_MP_Register.ps1

--- RAS_Azure_MP_Register.ps1
+++ RAS_Azure_MP_Register.ps1
@@ -141,8 +141,8 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Componen
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' -Name 'IsInstalled' -Value 0
 
 # Disable Edge first run experience
-New-item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Force
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Name 'HideFirstRunExperience' -Value 1 -Force
+New-item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Force | Out-Null
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Edge' -Name 'HideFirstRunExperience' -Value 1 -Force | Out-Null
 
 
 if (-not (IsSupportedOS)) {
