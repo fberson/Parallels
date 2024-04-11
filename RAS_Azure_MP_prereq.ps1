cf58e09a5671a200ed6e3c4b35a577e20aee9449
Freek Berson <fberson@ziggo.nl>
2024-04-11 11:32:39 +0200

    Update RAS_Azure_MP_prereq.ps1

--- RAS_Azure_MP_prereq.ps1
+++ RAS_Azure_MP_prereq.ps1
@@ -4,8 +4,8 @@
 .NOTES  
     File Name  : RAS_Azure_MP_prereq.ps1
     Author     : Freek Berson
-    Version    : v0.0.13
-    Date       : March 26 2024
+    Version    : v0.0.15
+    Date       : March 28 2024
 .EXAMPLE
     .\RAS_Azure_MP_prereq.ps1
 #>
@@ -15,40 +15,69 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$domainJoinUserName
 )
-
-
-#Set variables
-$installPath = "C:\install"
-
-#Set Windows Update to "Download Only" to prevent automatic installation of updates
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value 2
-
-# Check if the install path already exists
-if (-not (Test-Path -Path $installPath)) { New-Item -Path $installPath -ItemType Directory }
-
-#Configute logging
-$Logfile = "C:\install\RAS_Azure_MP_SG_prereq.log"
 function WriteLog {
     Param ([string]$LogString)
     $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
     $LogMessage = "$Stamp $LogString"
     Add-content $LogFile -value $LogMessage
 }
+function Add-firewallRules {
+    WriteLog "Configuring Firewall Rules"
+    New-NetFirewallRule -DisplayName "Parallels RAS Administration (TCP)" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 68, 80, 81, 1234, 135, 443, 445, 20001, 20002, 20003, 20009, 20020, 20030, 20443, 30004, 30006
+    New-NetFirewallRule -DisplayName "Parallels RAS Administration (UDP)" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 80, 443, 20000, 20009, 30004, 30006
+}
 
-#Create Firewall Rules
-WriteLog "Configuring Firewall Rules"
-New-NetFirewallRule -DisplayName "Parallels RAS Administration (TCP)" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 68, 80, 81, 1234, 135, 443, 445, 20001, 20002, 20003, 20009, 20020, 20030, 20443, 30004, 30006
-New-NetFirewallRule -DisplayName "Parallels RAS Administration (UDP)" -Direction Inbound -Action Allow -Protocol UDP -LocalPort 80, 443, 20000, 20009, 30004, 30006
+function Enable-remoteInstallRASAgent {
+    WriteLog "Disable UAC & Sharing Wizard"
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn -Name CheckedValue -Value 0
+}
+
+function Add-LocalGroupMember {
+    WriteLog "Adding Parallels RAS install user to local administrators group"
+    Add-LocalGroupMember -Group "Administrators" -Member $domainJoinUserName
+}
+
+#Set variables
+$installPath = "C:\install"
+$Logfile = "C:\install\RAS_Azure_MP_SG_prereq.log"
+
+# Check if the install path already exists and create it
+if (-not (Test-Path -Path $installPath)) { New-Item -Path $installPath -ItemType Directory }
+
+#Set Windows Update to "Download Only" to prevent automatic installation of updates
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value 2
+
+# Create Firewall Rules
+try {
+    Add-firewallRules
+}
+Catch {
+    Write-Host "ERROR: creating firewall rules"
+    Write-Host $_.Exception.Message
+    exit
+}
 
 #Disable UAC & Sharing Wizard to allow Remote Install of RAS Agent
-WriteLog "Disable UAC & Sharing Wizard"
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\SharingWizardOn -Name CheckedValue -Value 0
+try {
+    Enable-remoteInstallRASAgent
+}
+Catch {
+    Write-Host "ERROR: Disabling UAC & Sharing Wizard"
+    Write-Host $_.Exception.Message
+    exit
+}
 
 # Add Parallels RAS install user to local administrators group
-WriteLog "Adding Parallels RAS install user to local administrators group"
-Add-LocalGroupMember -Group "Administrators" -Member $domainJoinUserName
+try {
+    Add-LocalGroupMember
+}
+Catch {
+    Write-Host "ERROR: Adding Parallels RAS install user to local administrators group"
+    Write-Host $_.Exception.Message
+    exit
+}
 
 #Reboot the server to apply all changes
 shutdown /r /t 0
